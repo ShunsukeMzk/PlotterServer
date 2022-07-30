c46e164f588e629a1e59afebd242228f94382def
shunsuke <shunsuke.mzk@gmail.com>
2022-07-30 15:54:11 +0900

    update

--- plt/cli.py
+++ plt/cli.py
@@ -1,11 +1,12 @@
 import asyncio
-import os
+import click
 import json
+import os
 import signal
 import sys
-
-import click
 import websockets
+from datetime import datetime
+from pathlib import Path
 
 PLT_HOST = os.getenv("PLT_HOST", "localhost")
 PLT_PORT = os.getenv("PLT_PORT", "8080")
@@ -27,6 +28,13 @@ def cli():
 
 @cli.command()
 def server():
+    log = Path("plotter.log")
+    if log.exists():
+        mtime = datetime.fromtimestamp(log.stat().st_mtime)
+        mtime = mtime.replace(microsecond=0)
+        log.rename(Path(f"plotter.{mtime.isoformat()}.log"))
+    log = log.open(mode="w")
+
     reconnect_time = 10
 
     players = {}  # name: websocket
@@ -57,15 +65,27 @@ def server():
                 continue
             if p == "_sender":
                 continue
+            if p in disconnected:
+                continue
             await _send(p, message)
 
     async def _task(name):
         websocket = players[name]
         async for message in websocket:
             try:
+                message = message.rstrip("\n")
+                log.write(f"{message}\n")
+                log.flush()
                 message = json.loads(message)
                 click.echo("#", err=True, nl=False)
                 await _broadcast(name, message)
+
+                if message["type"] == "Action/Player/Leave":
+                    _name = message["playerName"]
+                    if _name in players:
+                        click.echo(f"Leave: {_name}", err=True)
+                        del players[_name]
+
             except json.decoder.JSONDecodeError:
                 click.echo(f"Invalid JSON message: {message}", err=True)
 
@@ -94,6 +114,9 @@ def server():
 
     async def _disconnect(name):
         click.echo(f"Disconnect: {name}", err=True)
+
+        if name not in players:
+            return
 
         if name in disconnected:
             disconnected[name].cancel()
@@ -130,32 +153,41 @@ def server():
         loop = asyncio.get_running_loop()
         stop = loop.create_future()
         loop.add_signal_handler(signal.SIGTERM, stop.set_result, None)
-        async with websockets.serve(_connect, "0.0.0.0", int(PLT_PORT), ping_timeout=None):
+        async with websockets.serve(
+                _connect,
+                "0.0.0.0",
+                int(PLT_PORT),
+                ping_interval=None,
+                ping_timeout=None
+        ):
             await stop
 
     asyncio.run(_serve())
 
 
 @cli.command()
-def observer():
-    uri = f"ws://{PLT_HOST}:{PLT_PORT}/_observer"
+@click.option("--file", "-f", type=click.File("a"), default=sys.stdout)
+def receiver(file):
+    uri = f"ws://{PLT_HOST}:{PLT_PORT}/_receiver"
 
     @interrupt
-    async def _observe():
+    async def _receive():
         async for websocket in websockets.connect(uri, ping_timeout=None):
             click.echo(f"Connected", err=True)
             try:
                 while True:
                     message = await websocket.recv()
-                    click.echo(message, nl=False)
+                    file.write(f"{message}\n")
+                    file.flush()
             except websockets.ConnectionClosed:
                 click.echo(f"Connection Closed and Retry Connect", err=True)
 
-    asyncio.run(_observe())
+    asyncio.run(_receive())
 
 
 @cli.command()
-def sender():
+@click.option("--file", "-f", type=click.File("r"), default=sys.stdin)
+def sender(file):
     uri = f"ws://{PLT_HOST}:{PLT_PORT}/_sender"
 
     @interrupt
@@ -168,7 +200,7 @@ def sender():
                 if last_message:
                     await websocket.send(last_message)
                     last_message = ""
-                for line in sys.stdin:
+                for line in file:
                     if not line:
                         continue
                     last_message = line
